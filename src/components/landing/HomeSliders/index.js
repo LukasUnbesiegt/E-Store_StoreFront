d94c34f1b1c24ff94dab4e266dbe91a196fa086c
Lukas Unbesiegt <blackhelios.web@gmail.com>
2019-08-22 16:24:17 +0630

    change home sliders

--- src/components/landing/HomeSliders/index.js
+++ src/components/landing/HomeSliders/index.js
@@ -1,22 +1,37 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import Slider from "react-slick";
 import { connect } from "react-redux";
 import { push } from "connected-react-router";
 import { slidersSample } from "../../seeds/contents";
-
+import Carousel, { Dots } from "@brainhubeu/react-carousel";
+import "@brainhubeu/react-carousel/lib/style.css";
 class index extends Component {
+	state = {
+		value: 0
+	};
+	onChange = value => this.setState({ value });
 	renderImages = () => {
 		const { sliders } = this.props;
 		if (sliders && sliders.length > 0) {
 			return sliders.map(({ image, collection }) => {
+				console.log("sliders", image);
 				return (
 					<img
 						src={image}
-						className="img-fluid"
 						style={{
+							backgroundPosition: "center",
+							backgroundSize: "cover",
+							backgroundRepeat: "no-repeat",
 							cursor: "pointer",
-							objectFit: "cover"
+							objectFit: "fill",
+							width: "100%",
+
+							maxHeight:
+								this.props.browser.orientation === "portrait"
+									? "240px"
+									: "500px"
 						}}
+						className="img-fluid"
 						onClick={() => {
 							this.props.getProductsForShop(1, { collections: [collection] });
 						}}
@@ -28,10 +43,14 @@ class index extends Component {
 				return (
 					<img
 						src={image}
-						className="img-fluid"
+						className=""
 						style={{
+							backgroundPosition: "center",
+							backgroundSize: "cover",
+							backgroundRepeat: "no-repeat",
 							cursor: "pointer",
-							objectFit: "cover"
+							objectFit: "cover",
+							width: "100%"
 						}}
 						onClick={() => {
 							this.props.getProductsForShop(1, { collections: [collection] });
@@ -71,12 +90,25 @@ class index extends Component {
 			]
 		};
 
-		return <Slider {...settings}>{this.renderImages()}</Slider>;
+		return (
+			<Fragment>
+				<Carousel autoPlay={2300} animationSpeed={2000} infinite>
+					{this.renderImages()}
+				</Carousel>
+				<Dots
+					value={this.state.value}
+					onChange={this.onChange}
+					number={this.props.sliders ? this.props.sliders.length : 4}
+				/>
+				{/* <Slider {...settings}>{this.renderImages()}</Slider>; */}
+			</Fragment>
+		);
 	}
 }
 
 const mapStateToProps = state => ({
-	sliders: state.contents.sliders ? state.contents.sliders.sliders : []
+	sliders: state.contents.sliders ? state.contents.sliders.sliders : [],
+	browser: state.browser
 });
 
 const mapDispatchToProps = {
